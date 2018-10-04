1e5fc56863dfe6a694712d94a4d00d708b4e2128
nageseio <37033276+nageseio@users.noreply.github.com>
2018-10-04 19:14:50 -0300

    Create Caracteres.py

---
+++ Roteiro 4 - Strings/Caracteres.py
@@ -0,0 +1,16 @@
+x=100000
+c=0
+i=0
+lista=[]
+
+while x > 0:
+  n=int(input())
+  if n == 0:
+    break
+  lista = [str(i) for i in input()]
+  lista.reverse()
+  for v in range(0,n):
+    print(lista[i],end="")
+    i+=1
+  print("")
+  i=0
