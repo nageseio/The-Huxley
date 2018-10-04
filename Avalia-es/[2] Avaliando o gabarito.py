3ed8404ca8a3182c923f50e10008503cbd3cb483
nageseio <37033276+nageseio@users.noreply.github.com>
2018-10-04 19:28:00 -0300

    Create [2] Avaliando o gabarito.py

---
+++ Avalia-es/[2] Avaliando o gabarito.py
@@ -0,0 +1,40 @@
+b=1
+pontos=0
+l=0
+l2=0
+l3=0
+c=0
+qnt=0
+qnt_a=0
+qnt3=0
+pg=0
+listan=[]
+gabarito=[str(i) for i in input()]
+while b>0:
+  nome_a = input()
+  qnt_a+=1
+  if nome_a == "sair":
+    break
+  notas=[str(i) for i in input()]
+  for i in range(0,5):
+    if gabarito[l] == notas[l2]:
+      pontos+=20
+    l+=1
+    l2+=1
+  listan.append(pontos)
+  pg+=pontos
+  if pontos > 60:
+    qnt+=1
+  if pontos >= c:
+    n1 = nome_a
+    c = pontos
+  media_g = pg/qnt_a
+  pontos=0
+  l=0
+  l2=0
+for i in range (0, qnt_a-1):
+  if media_g < listan[l3]:
+    qnt3+=1
+  l3+=1
+print(qnt3)
+print(n1)
