df1485e98d80fdc58d1fc25e57497f89b806e2db
nageseio <37033276+nageseio@users.noreply.github.com>
2018-10-07 17:56:23 -0300

    Update [1] O Maior.py

--- Roteiro 5 - Funcoes/[1] O Maior.py
+++ Roteiro 5 - Funcoes/[1] O Maior.py
@@ -1,8 +1,10 @@
+def listas(lista):
+  lista=[]
+  l=0
+  for i in range(0, 3):
+    lista.append(n[l])
+    l+=1
+  lista.sort()
+  return lista
 n=[int(i) for i in input().split()]
-lista=[]
-l=0
-for i in range(0, 3):
- lista.append(n[l])
- l+=1
-lista.sort()
-print(lista[2],"eh o maior")
+print(listas(n)[2])
