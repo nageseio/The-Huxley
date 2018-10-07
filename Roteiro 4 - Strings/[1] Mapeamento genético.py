015df8c4bbb7c11648f42a047935eb4dd6f2bd49
nageseio <37033276+nageseio@users.noreply.github.com>
2018-10-06 22:04:03 -0300

    Create [1] Mapeamento genético.py

---
+++ Roteiro 4 - Strings/[1] Mapeamento genético.py
@@ -0,0 +1,21 @@
+dna=input()
+dna2=input()
+li=[]
+cc=2
+if dna2 in dna:
+  while cc <  99:
+    if 0 in li:
+      break
+    c1 = dna.count(dna2*cc)
+    li.append(c1)
+    cc+=1
+else:
+  print("ERRO")
+
+if len(li) > 1:
+  cc = cc-2
+  print(dna.find(dna2*cc))
+  print(len(li)) 
+elif len(li) == 1:
+ print(0)
+ print(1)
