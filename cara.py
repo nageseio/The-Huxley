cc0f7b3868a9f5a9e71ff7119ffe7aead4a6c8d5
nageseio <37033276+nageseio@users.noreply.github.com>
2018-11-06 02:17:32 -0200

    Update cara.py

--- cara.py
+++ cara.py
@@ -1,45 +1,29 @@
-var=[int(x) for x in input().split()]
-co=0
-def Listbin(n,co):
-    listbin = []
-    for i in range(2**n):
-        li = []
-        bini = str('{0:b}'.format(i))
-        for x in range(len(bini)):
-            li.append(int(bini[x]))
-        listbin.append(li)
-        while len(listbin[i]) < n:
-            listbin[i].insert(0,0)
-    if 2**n == co:
-        return listbin
-    co+=1
-    return Listbin(n,co)
-
-bini=Listbin(var[0],co)
-bini2=bini[:]
-for i in bini:
-    qntc=i.count(0)
-    qntk=i.count(1)
-    if abs(qntc-qntk) != var[1]:
-        ind=bini2.index(i)
-        del(bini2[ind])
-for i in bini:
-    for i2 in range(len(i)):
-        if i[i2] == 0:
-            i[i2]='C'
-        else:
-            i[i2]='K'
-x=(len(bini2))
-print(x)
-f=0
-i=0
-def Diferenca(lista,f,i,x):
-    if x == i:
-        return f
-    k=lista[i].count('K')
-    c=lista[i].count('C')
-    if abs(c-k) == var[1]:
-        f+=1
-    i+=1
-    return Diferenca(lista,f,i,x)
-#print(Diferenca(bini,f,i,x))
+var=[int(x) for x in input().split()]#ENTRADA
+lista=['C','K']#MULTIPLICADOR
+va=var[0]# QUANTIDADE DE MOEDAS
+lt=[]#LISTA ONDE FICAR ARMAZENADA AS PROBABILIDADES
+Soma=0#CONATADOR DIFERENCA
+D=var[1]# DIFERENCA DESEJADA
+def sum(lista,var,lt):
+    ck=['C','K']
+    if var > 1:
+        lt=[]
+        for k in range(2):
+            l=[]
+            for i in range(len(lista)):
+                x=ck[k]+lista[i]
+                l.insert(0, x)
+            for i in range(len(l)):
+                lt.append(l[i])
+    if len(lt[0]) == var:
+        return lt# RETORNO
+    return sum(lt,var,lt)# RECURSAO
+ll=sum(lista,va,lt)
+def Contador(lista2,Soma,D): #lista2 exemplo = [[C,C,C], [C,C,K], [C,K,C], [C,K,K], [K,C,C], [K,C,K], [K,K,C], [K,K,K]]
+    for l in lista2:
+        ck=l.count('K')
+        cc=l.count('C')
+        if abs(ck-cc) == D:# DIFERENCA COM MODULO=(abs), caso a diferenca seja negativa
+            Soma+=1
+    return Soma
+print(Contador(ll,Soma,D))
