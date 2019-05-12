eb44618b0ecd61e62438a37e2df5ef61b245a579
nageseio <37033276+nageseio@users.noreply.github.com>
2019-05-12 02:17:00 -0300

    Update New.py

--- Projeto/New.py
+++ Projeto/New.py
@@ -1,29 +1,29 @@
-def QuantidadeVariaveis(Entrada):#Determina a quantida de variaveis a partir da Entrada.
-    i=1
+def quantidadeVariaveis(Entrada):#Determina a quantida de variaveis a partir da Entrada.
+    n=1
     while True:
-        if 2**i == len(Entrada):
-            return i
-        elif 2**i > len(Entrada):
+        if 2**n == len(Entrada):
+            return n
+        elif 2**n > len(Entrada):
             return 0
-        i+=1
+        n+=1
 
 def criarListaBinario(Var,ums):#Cria a lista em binaro com base na quantidade de variaveis e descartas os termos irrevelantes.
-    listbin = []
-    listbin2=[]
+    listabin = []
+    listabin2=[]
     for i in range(2**Var):
         li = []
-        bini = str('{0:b}'.format(i))
-        for x in range(len(bini)):
-            li.append(int(bini[x]))
-        listbin.append(li)
-        while len(listbin[i]) < Var:
-            listbin[i].insert(0,0)
-        listbin[i].insert(0, [str(i)])
-        listbin[i].insert(0, str(' '))
+        bin = str('{0:b}'.format(i))
+        for x in range(len(bin)):
+            li.append(int(bin[x]))
+        listabin.append(li)
+        while len(listabin[i]) < Var:
+            listabin[i].insert(0,0)
+        listabin[i].insert(0, [str(i)])
+        listabin[i].insert(0, str(' '))
     for i in range(len(ums)):
         if ums[i] == '1':
-            listbin2.append(listbin[i])
-    return listbin2
+            listabin2.append(listabin[i])
+    return listabin2
 
 def SepararUm1(Lista, Cont, ListaTermos, MAX):#Sepera os termos em listas diferentes, pela quantidade de 1's em cada termo.
     l=[]
@@ -40,22 +40,20 @@ def SepararUm1(Lista, Cont, ListaTermos, MAX):#Sepera os termos em listas difere
     Cont+=1
     return SepararUm1(Lista, Cont, ListaTermos, MAX)
 
-
-def Comparar0(L1,L2):#Compara dois termos e verifica se a comparação e o termo obitido será valido
+def Comparar0(termo1,termo2):#Compara dois termos e verifica se a comparação e o termo obitido será valido
     L=[' ', []]
     M=0
-    for i in range(2,len(L1)):
-        if L1[i] != L2[i]:
+    for i in range(2,len(termo1)):
+        if termo1[i] != termo2[i]:
             L.append('-')
             M+=1
         else:
-            L.append(L1[i])
+            L.append(termo1[i])
     if M == 1:
-        L[1] = L1[1]+L2[1]
+        L[1] = termo1[1]+termo2[1]
         return L
     else:
         return []
-
 
 def Comparar(LB):#Seleciona os termos que serao comparados, e retorna uma lista contendo as comparaçoes.
     L=[]
@@ -73,26 +71,26 @@ def Comparar(LB):#Seleciona os termos que serao comparados, e retorna uma lista 
                 L.append(n2)
     return L
 
-def MinimizadorVerificador(Contador, ListaVazia, L):#função secundaria de Minimizador, para determinar uma parada valida
+def MinimizadorVerificador(Contador, ListaVazia, L, qntvar):#função secundaria de Minimizador, para determinar uma parada valida
     Contador,ListaVazia = 0,[]
-    Lista = SepararUm1(L, Contador, ListaVazia, QuantidadeVariaveis)
+    Lista = SepararUm1(L, Contador, ListaVazia, qntvar)
     Lista2 = Comparar(Lista)
     return Lista2
 
-def Minimizador(Contador, ListaVazia, L):#Usa as funcoes criadas para manipular os termos e retornar elementos validos, que serao usados para formar o mapa dos implicantes(termos).
+def Minimizador(Contador, ListaVazia, L, qntvar):#Usa as funcoes criadas para manipular os termos e retornar elementos validos, que serao usados para formar o mapa dos implicantes(termos).
     Contador,ListaVazia = 0,[]
-    Lista = SepararUm1(L, Contador, ListaVazia, QuantidadeVariaveis)
+    Lista = SepararUm1(L, Contador, ListaVazia, qntvar)
     Lista2 = Comparar(Lista)
     Contador,ListaVazia = 0,[]
-    M = MinimizadorVerificador(Contador, ListaVazia, Lista2)
+    M = MinimizadorVerificador(Contador, ListaVazia, Lista2, qntvar)
     if M != Lista2:
-        return Minimizador(Contador, ListaVazia, Lista2)
+        return Minimizador(Contador, ListaVazia, Lista2, qntvar)
     else:
         return Lista2
 
-def Diferenciador(I0,I1):#Verificar se dois termos são iguais.
-    if I0 != [] and I1 != []:
-        if I0[2::] == I1[2::]:
+def Diferenciador(termo0,termo1):#Verificar se dois termos são iguais.
+    if termo0 != [] and termo1 != []:
+        if termo0[2::] == termo1[2::]:
             return True
         else:
             return False
@@ -154,7 +152,6 @@ def essentialPrimeImplicants(mapa):#Encontra os termos essenciais,implicantes pr
         l2.append(l)
     return l2
 
-
 def Multiplicador(Termo1,Termo2):#Multiplica variaveis em listas, retorna resultadoda multiplicação
     M=[]
     for i in Termo1:
@@ -166,8 +163,6 @@ def Multiplicador(Termo1,Termo2):#Multiplica variaveis em listas, retorna result
                 if x not in M:
                     M.append(x)
     return M
-
-
 
 def ExtracaoPetrick(Lista):#Retorna um lista contendo as possibilidas, da expressao minimizada
     ListaExt=[]
@@ -230,9 +225,7 @@ def SubstituirVaririaveis(listadeimplicantes,termosessenciais):#Faz a substituic
                 listavariaveissub+=[listavariaveis]
     return listavariaveissub
 
-
-
-def OrganizarVariaveis(variaveis):
+def OrganizarVariaveis(variaveis):#Organiza as variaveis, adicionas as expressoes de soma e multiplicação, se necessario
     l=[]
     c=0
     for i in range(len(variaveis)):
@@ -245,22 +238,19 @@ def OrganizarVariaveis(variaveis):
             l.append(variaveis[i][i2])
     return "".join(l)
 
+def Comecar(Entrada):
+    QuantidadeVariaveis = quantidadeVariaveis(Entrada)
+    ListaA = criarListaBinario(QuantidadeVariaveis,Entrada)
+    contador,ListaTermos = 0,[]
+    ListaB = Minimizador(contador, ListaTermos, ListaA, QuantidadeVariaveis)
+    ListaC = LimparRepetidos(ListaB)
+    Mapa = criarMapa(ListaC,QuantidadeVariaveis)
+    PrimosEssenciais = essentialPrimeImplicants(Mapa)
+    combinacoesPetrick = ExtracaoPetrick(PrimosEssenciais)
+    TermoReduzido = EncontrarExpressao(combinacoesPetrick,Mapa)
+    TermosEssenciais = VerificarTermosMapa(TermoReduzido,Mapa)
+    Variaveis = SubstituirVaririaveis(ListaC,TermosEssenciais)
+    TermoMinimizado = OrganizarVariaveis(Variaveis)
+    print(TermoMinimizado)
 
-
-
-
-Entrada = input().split()
-QuantidadeVariaveis = QuantidadeVariaveis(Entrada)
-ListaA = criarListaBinario(QuantidadeVariaveis,Entrada)
-cont,ListaTermos = 0,[]
-ListaB = Minimizador(cont, ListaTermos, ListaA)
-ListaC = LimparRepetidos(ListaB)
-Mapa = criarMapa(ListaC,QuantidadeVariaveis)
-PrimosEssenciais = essentialPrimeImplicants(Mapa)
-combinacoesPetrick = ExtracaoPetrick(PrimosEssenciais)
-TermoReduzido = EncontrarExpressao(combinacoesPetrick,Mapa)
-TermosEssenciais = VerificarTermosMapa(TermoReduzido,Mapa)
-Variaveis = SubstituirVaririaveis(ListaC,TermosEssenciais)
-TermoMinimizado = OrganizarVariaveis(Variaveis)
-
-print(TermoMinimizado)
+Comecar(input().split())
