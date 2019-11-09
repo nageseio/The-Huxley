eb7c44c8bfe9f9c11e8963a9de52c02df650fdad
nageseio <37033276+nageseio@users.noreply.github.com>
2019-11-09 12:23:26 -0300

    Update [1]  Superdigito.py

--- Roteiro 7 - Recursividade/[1]  Superdigito.py
+++ Roteiro 7 - Recursividade/[1]  Superdigito.py
@@ -1,11 +1,15 @@
-D=[str(x) for x in input().split()]
-D=D[0]*int(D[1])
-def Superdigito(li):
-    lis=[]
-    for i in range(len(li)):
-        lis.append(int(li[i]))
-    l=str(sum(lis))
-    if len(l) == 1:
-        return l
-    return Superdigito(l)
-print(Superdigito(D))
+def superDigito(sDigito):
+    listDigitos = []
+    
+    for e in sDigito:
+        listDigitos.append(int(e))
+        
+    sDigito = str(sum(listDigitos))
+    
+    if len(sDigito) == 1:
+        return sDigito
+    return superDigito(sDigito)
+
+digito = input().split()
+digito = digito[0]*int(digito[1])
+print(superDigito(digito))
